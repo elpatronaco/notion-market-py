69f3a82865bf1bb60f6c9cecc8c5c065a71fdba4
Pau Colomé <paucolome10@gmail.com>
2024-11-10 12:26:22 +0100

    feat: remove unused env

--- env.py
+++ env.py
@@ -1,9 +1,7 @@
 import os
 
 class Env:
-    TOKEN_V2 = os.environ.get("TOKEN_V2")
+    NOTION_TOKEN_V2 = os.environ.get("NOTION_TOKEN_V2")
     DATABASE_ID = os.environ.get("DATABASE_ID")
-    ALPACA_API_KEY = os.environ.get("ALPACA_API_KEY")
-    ALPACA_SECRET_KEY = os.environ.get("ALPACA_SECRET_KEY")
     CRYPTO_API_KEY = os.environ.get("CRYPTO_API_KEY")
     CRYPTO_SECRET_KEY = os.environ.get("CRYPTO_SECRET_KEY")
